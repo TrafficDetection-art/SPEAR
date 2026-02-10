5b3dbfec60aba4bff6409bdcf971aac495ec068e
TrafficDetection-art <sunpeishuai@iie.ac.cn>
2026-02-10 15:29:29 +0800

    Update 1.py

--- DL/1.py
+++ DL/1.py
@@ -1,6 +1,1 @@
-import torch
 
-print("CUDA available:", torch.cuda.is_available())
-print("Device count:", torch.cuda.device_count())
-if torch.cuda.is_available():
-    print("GPU name:", torch.cuda.get_device_name(0))
